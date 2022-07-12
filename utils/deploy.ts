6054548a30da2920bf8f7e390f38569cfe541465
rust-0x0 <rust.0x0@gmail.com>
2022-07-12 17:49:01 +0800

    update

--- utils/deploy.ts
+++ utils/deploy.ts
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const basepath = "/Users/lisheng/mygit/rust0x0/5degrees-protocol-substrate/"
+const basepath = "/Users/lisheng/mygit/rust0x0/hex-space-protocol-substrate/"
 async function execCmd(cmd: String, keyword: String, path: String) {
     let { err, stdout, stderr } = await exec(cmd, { cwd: path });
     if (err) {
@@ -45,14 +45,14 @@ async function getjson(key: String, section: String) {
 }
 const execpath = " /Users/lisheng/Downloads/cargo-contract-1.1.0/target/release/"
 async function upload(acc: String, path: String, key: String) {
-    const output = await execCmd('cargo-contract contract upload --suri //' + acc, "Code hash", path);
+    const output = await execCmd('cargo contract upload --suri //' + acc, "Code hash", path);
     savejson(key, output, "hash");
     return output;
 }
 async function instantiate(acc: String, args: String, code_hash: String, path: String, key: String) {
     // let output = await execCmd('cargo-contract contract instantiate --constructor new ' + (args == undefined || args == "" ? "" : " --args " + args) + ' --suri //' + acc + ' --code-hash ' + code_hash, "Contract ", path);
     console.log("instantiate paras=",'cargo-contract contract instantiate --constructor new ' + (args == undefined || args == "" ? "" : " --args " + args) + '  --suri //' + acc, "Contract ", path);
-    let output = await execCmd('cargo-contract contract instantiate --constructor new ' + (args == undefined || args == "" ? "" : " --args " + args) + '   --suri //' + acc, "Contract ", path);
+    let output = await execCmd('cargo contract instantiate --constructor new ' + (args == undefined || args == "" ? "" : " --args " + args) + '  --storage-deposit-limit 20000000000000000  --suri //' + acc, "Contract ", path);
 
     savejson(key, output, "address");
     return output;
@@ -61,9 +61,9 @@ async function instantiate(acc: String, args: String, code_hash: String, path: S
     if (true) {
         const users = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Ferdie"];
         let pathes = {
-            "erc1155": basepath + "erc1155", "five_degrees": basepath + "five_degrees"
+            "erc1155": basepath + "erc1155", "hex_space": basepath + "hex_space"
         };
-        let args = { "erc1155": "", "five_degrees": "'5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL 0xd5f768bbb70d4afc86b404fd767f01a64e4d0ab7f2bd6cd400895adbe79491c2'" }
+        let args = { "erc1155": "", "hex_space": "'5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL 0xd5f768bbb70d4afc86b404fd767f01a64e4d0ab7f2bd6cd400895adbe79491c2'" }
         let keys = Object.keys(pathes);//.slice(0,7);
 
         let codehashes: any = [];
@@ -77,7 +77,7 @@ async function instantiate(acc: String, args: String, code_hash: String, path: S
                 console.log(i, "======code_hash======", code_hash);
 
                 codehashes.push(code_hash);
-                args["five_degrees"] = "1 " + code_hash + "";
+                args["hex_space"] = "1 " + code_hash + "";
             } else {
                 const contract_address = await instantiate(users[i], args[key as keyof typeof args], "code_hash", pathes[key as keyof typeof pathes], key);
                 contractaddresses.push(contract_address);
